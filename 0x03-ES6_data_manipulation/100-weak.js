331259e190f31781fb502e0aee8b773c2e6c4282
Bahy Allam <66816838+BahyAllaAllam@users.noreply.github.com>
2024-07-01 19:41:02 +0300

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,17 +1,11 @@
 export const weakMap = new WeakMap();
 
-
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-  
-  const count = weakMap.get(endpoint) + 1;
-  weakMap.set(endpoint, count);
-  
-  if (count >= 5) {
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-  
-  return count;
 }
