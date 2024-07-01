a9638e75a924e6bce3061b37cb8b2fbd94184b30
Bahy Allam <66816838+BahyAllaAllam@users.noreply.github.com>
2024-07-01 19:48:52 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,20 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
+  if (
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+  ) {
     return '';
   }
   
   const result = [];
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+  
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSlicing = value.substring(startString.length);
+      
+      if (valueSlicing && valueSlicing !== value) {
+        result.push(valueSlicing);
+      }
     }
   }
-  
   return result.join('-');
 }
